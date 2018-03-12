0f339d97ef0cb9441956476808dcfa9d70a7316e
deimors <deimors@gmail.com>
2018-03-11 19:06:11 -0600

    Ordering Position traversal by Direction; tests passing

---
+++ 2048EventBased.Tests/MovementTests.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using FakeItEasy;
+using Xunit;
+
+namespace _2048EventBased.Tests
+{
+	public class MovementTests
+	{
+		public class When2At00
+		{
+			private readonly Game sut = new Game { [0, 0] = 2 };
+			private readonly Action<NumberMovedEvent> numberMovedListener = A.Fake<Action<NumberMovedEvent>>();
+
+			public When2At00()
+			{
+				sut.NumberMoved += numberMovedListener;
+			}
+
+			[Fact]
+			public void MoveRight_NumberMovedInvokedWith2MovedFrom00To03()
+			{
+				sut.Move(Direction.Right);
+
+				A.CallTo(() => numberMovedListener.Invoke(new NumberMovedEvent(2, 0, 0, 0, 3))).MustHaveHappened();
+			}
+
+			[Fact]
+			public void MoveDown_NumberMovedInvokedWith2MovedFrom00To30()
+			{
+				sut.Move(Direction.Down);
+
+				A.CallTo(() => numberMovedListener.Invoke(new NumberMovedEvent(2, 0, 0, 3, 0))).MustHaveHappened();
+			}
+
+			[Fact]
+			public void MoveLeft_NumberMovedNotInvoked()
+			{
+				sut.Move(Direction.Left);
+
+				A.CallTo(() => numberMovedListener.Invoke(A<NumberMovedEvent>._)).MustNotHaveHappened();
+			}
+
+			[Fact]
+			public void MoveUp_NumberMovedNotInvoked()
+			{
+				sut.Move(Direction.Up);
+
+				A.CallTo(() => numberMovedListener.Invoke(A<NumberMovedEvent>._)).MustNotHaveHappened();
+			}
+		}
+		
+		public class When2At33
+		{
+			private readonly Game sut = new Game { [3, 3] = 2 };
+			private readonly Action<NumberMovedEvent> numberMovedListener = A.Fake<Action<NumberMovedEvent>>();
+
+			public When2At33()
+			{
+				sut.NumberMoved += numberMovedListener;
+			}
+
+			[Fact]
+			public void MoveUp_NumberMovedInvokedWith2MovedFrom33To03()
+			{
+				sut.Move(Direction.Up);
+
+				A.CallTo(() => numberMovedListener.Invoke(new NumberMovedEvent(2, 3, 3, 0, 3))).MustHaveHappened();
+			}
+
+			[Fact]
+			public void MoveLeft_NumberMovedInvokedWith2MovedFrom33To30()
+			{
+				sut.Move(Direction.Left);
+
+				A.CallTo(() => numberMovedListener.Invoke(new NumberMovedEvent(2, 3, 3, 3, 0))).MustHaveHappened();
+			}
+
+			[Fact]
+			public void MoveDown_NumberMovedNotInvoked()
+			{
+				sut.Move(Direction.Down);
+
+				A.CallTo(() => numberMovedListener.Invoke(A<NumberMovedEvent>._)).MustNotHaveHappened();
+			}
+
+			[Fact]
+			public void MoveRight_NumberMovedNotInvoked()
+			{
+				sut.Move(Direction.Right);
+
+				A.CallTo(() => numberMovedListener.Invoke(A<NumberMovedEvent>._)).MustNotHaveHappened();
+			}
+		}
+
+
+		public class When2At00And33And4At03And30
+		{
+			private readonly Game sut = new Game
+			{
+				[0, 0] = 2,
+				[3, 3] = 2,
+				[0, 3] = 4,
+				[3, 0] = 4
+			};
+
+			private readonly Action<NumberMovedEvent> numberMovedListener = A.Fake<Action<NumberMovedEvent>>();
+
+			public When2At00And33And4At03And30()
+			{
+				sut.NumberMoved += numberMovedListener;
+			}
+
+			[Fact]
+			public void MoveUp_NumberMovedInvokedWith2MovedFrom33To13AndWith4MovedFrom30To10()
+			{
+				sut.Move(Direction.Up);
+
+				A.CallTo(() => numberMovedListener.Invoke(new NumberMovedEvent(2, 3, 3, 1, 3))).MustHaveHappened();
+				A.CallTo(() => numberMovedListener.Invoke(new NumberMovedEvent(4, 3, 0, 1, 0))).MustHaveHappened();
+			}
+
+			[Fact]
+			public void MoveLeft_NumberMovedInvokedWith2MovedFrom33To31AndWith4MovedFrom03To01()
+			{
+				sut.Move(Direction.Left);
+
+				A.CallTo(() => numberMovedListener.Invoke(new NumberMovedEvent(2, 3, 3, 3, 1))).MustHaveHappened();
+				A.CallTo(() => numberMovedListener.Invoke(new NumberMovedEvent(4, 0, 3, 0, 1))).MustHaveHappened();
+			}
+
+			[Fact]
+			public void MoveDown_NumberMovedInvokedWith2MovedFrom00To20AndWith4MovedFrom03To23()
+			{
+				sut.Move(Direction.Down);
+
+				A.CallTo(() => numberMovedListener.Invoke(new NumberMovedEvent(2, 0, 0, 2, 0))).MustHaveHappened();
+				A.CallTo(() => numberMovedListener.Invoke(new NumberMovedEvent(4, 0, 3, 2, 3))).MustHaveHappened();
+			}
+
+			[Fact]
+			public void MoveRight_NumberMovedInvokedWith2MovedFrom00To02AndWith4MovedFrom30To32()
+			{
+				sut.Move(Direction.Right);
+
+				A.CallTo(() => numberMovedListener.Invoke(new NumberMovedEvent(2, 0, 0, 0, 2))).MustHaveHappened();
+				A.CallTo(() => numberMovedListener.Invoke(new NumberMovedEvent(4, 3, 0, 3, 2))).MustHaveHappened();
+			}
+		}
+
+		public class When2At01And4At02
+		{
+			private readonly Game sut = new Game
+			{
+				[0, 1] = 2,
+				[0, 2] = 4
+			};
+
+			private readonly Action<NumberMovedEvent> numberMovedListener = A.Fake<Action<NumberMovedEvent>>();
+
+			public When2At01And4At02()
+			{
+				sut.NumberMoved += numberMovedListener;
+			}
+
+			[Fact]
+			public void MoveRight_NumberMovedInvokedWith2MovedFrom01To02AndWith4MovedFrom02To03()
+			{
+				sut.Move(Direction.Right);
+
+				A.CallTo(() => numberMovedListener.Invoke(new NumberMovedEvent(2, 0, 1, 0, 2))).MustHaveHappened();
+				A.CallTo(() => numberMovedListener.Invoke(new NumberMovedEvent(4, 0, 2, 0, 3))).MustHaveHappened();
+			}
+
+			[Fact]
+			public void MoveLeft_NumberMovedInvokedWith2MovedFrom01To00AndWith4MovedFrom02To01()
+			{
+				sut.Move(Direction.Left);
+
+				A.CallTo(() => numberMovedListener.Invoke(new NumberMovedEvent(2, 0, 1, 0, 0))).MustHaveHappened();
+				A.CallTo(() => numberMovedListener.Invoke(new NumberMovedEvent(4, 0, 2, 0, 1))).MustHaveHappened();
+			}
+		}
+
+		public class When2At10And4At20
+		{
+			private readonly Game sut = new Game
+			{
+				[1, 0] = 2,
+				[2, 0] = 4
+			};
+
+			private readonly Action<NumberMovedEvent> numberMovedListener = A.Fake<Action<NumberMovedEvent>>();
+
+			public When2At10And4At20()
+			{
+				sut.NumberMoved += numberMovedListener;
+			}
+
+			[Fact]
+			public void MoveUp_NumberMovedInvokedWith2MovedFrom10To00AndWith4MovedFrom20To10()
+			{
+				sut.Move(Direction.Up);
+
+				A.CallTo(() => numberMovedListener.Invoke(new NumberMovedEvent(2, 1, 0, 0, 0))).MustHaveHappened();
+				A.CallTo(() => numberMovedListener.Invoke(new NumberMovedEvent(4, 2, 0, 1, 0))).MustHaveHappened();
+			}
+
+			[Fact]
+			public void MoveDown_NumberMovedInvokedWith2MovedFrom10To20AndWith4MovedFrom20To30()
+			{
+				sut.Move(Direction.Down);
+
+				A.CallTo(() => numberMovedListener.Invoke(new NumberMovedEvent(2, 1, 0, 2, 0))).MustHaveHappened();
+				A.CallTo(() => numberMovedListener.Invoke(new NumberMovedEvent(4, 2, 0, 3, 0))).MustHaveHappened();
+			}
+		}
+	}
+}
--- 2048EventBased/Game.cs
+++ 2048EventBased/Game.cs
@@ -9,11 +9,13 @@ namespace _2048EventBased
 	{
 		private readonly Maybe<int>[,] _cells = new Maybe<int>[4,4];
 
-		private Maybe<int> this[Position position] 
-			=> _cells[position.Row, position.Column];
+		private Maybe<int> this[Position position]
+		{
+			get => _cells[position.Row, position.Column];
+			set => _cells[position.Row, position.Column] = value;
+		}
 
-		private IEnumerable<Position> AllPositions
-			=> Enumerable.Range(0, _cells.GetLength(0)).SelectMany(row => Enumerable.Range(0, _cells.GetLength(1)).Select(column => new Position(row, column)));
+		private IEnumerable<Position> AllPositions => Enumerable.Range(0, _cells.GetLength(0)).SelectMany(row => Enumerable.Range(0, _cells.GetLength(1)).Reverse().Select(column => new Position(row, column)));
 
 		public event Action<NumberAddedEvent> NumberAdded;
 		public event Action<NumberMovedEvent> NumberMoved;
@@ -29,18 +31,42 @@ namespace _2048EventBased
 
 		public void Move(Direction direction)
 		{
-			foreach (var position in AllPositions)
+			foreach (var position in GetPositionsForDirection(direction))
 			{
 				this[position].Match(
 					number => FindMoveTarget(position, direction).Match(
-						target => NumberMoved?.Invoke(new NumberMovedEvent(number, position, target)),
+						target =>
+						{
+							this[target] = number.ToMaybe();
+							this[position] = Maybe<int>.Nothing;
+							NumberMoved?.Invoke(new NumberMovedEvent(number, position, target));
+						},
 						() => { }
 					),
 					() => { }
 				);
 			}
 		}
-		
+
+		private IEnumerable<Position> GetPositionsForDirection(Direction direction) 
+			=> RowsOrderedFor(direction)
+				.SelectMany(
+					row => ColumnsOrderedFor(direction)
+							.Select(column => new Position(row, column))
+				);
+
+		private IEnumerable<int> RowsOrderedFor(Direction direction)
+			=> RowsOrderedFor(direction, Enumerable.Range(0, _cells.GetLength(0)));
+
+		private static IEnumerable<int> RowsOrderedFor(Direction direction, IEnumerable<int> rows)
+			=> direction == Direction.Down ? rows.Reverse() : rows;
+
+		private IEnumerable<int> ColumnsOrderedFor(Direction direction)
+			=> ColumnsOrderedFor(direction, Enumerable.Range(0, _cells.GetLength(1)));
+
+		private static IEnumerable<int> ColumnsOrderedFor(Direction direction, IEnumerable<int> columns)
+			=> direction == Direction.Right ? columns.Reverse() : columns;
+
 		private readonly IReadOnlyDictionary<Direction, Position> _increments = new Dictionary<Direction, Position>
 		{
 			{ Direction.Down, new Position(1, 0) },
@@ -51,17 +77,17 @@ namespace _2048EventBased
 
 		private Maybe<Position> FindMoveTarget(Position origin, Direction direction)
 			=> this[origin].SelectOrElse(
-				number => SelectMoveTarget(GetMoveCandidates(origin, direction)),
+				number => SelectMoveTarget(GetMoveCandidates(origin, direction), number),
 				() => Maybe<Position>.Nothing
 			);
 
 		private IEnumerable<Position> GetMoveCandidates(Position origin, Direction direction)
 			=> origin.Project(_increments[direction]).TakeWhile(IsInBounds);
 
-		private Maybe<Position> SelectMoveTarget(IEnumerable<Position> moveCandidates)
+		private Maybe<Position> SelectMoveTarget(IEnumerable<Position> moveCandidates, int originNumber)
 			=> moveCandidates
 				.Pairwise()
-				.FirstMaybe(pair => this[pair.Item2].HasValue)
+				.FirstMaybe(pair => this[pair.Item2].SelectOrElse(value => value != originNumber, () => false))
 				.SelectOrElse(pair => pair.Item1.ToMaybe(), moveCandidates.LastMaybe);
 		
 		private bool IsInBounds(Position position)
